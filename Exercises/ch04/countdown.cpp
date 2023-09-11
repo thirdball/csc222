87c6d5e30d54871af3d0a6137ac3f0dd72917ae3
1026639 <ivanhughes5604@icloud>
2023-09-11 08:43:38 -0400

    exercise 1 infinite recursion

---
+++ Exercises/ch04/countdown.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void countdown(int n) {
+    if (n == 0) {
+        cout << "Blastoff!" << endl;
+    } else {
+        cout << n << endl;
+        countdown(n-1);
+    }
+}
+
+int main()
+{
+    countdown(-3);
+    return 0;
+}
+//the countdown decreases infinitely
