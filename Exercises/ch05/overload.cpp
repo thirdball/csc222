099ef63453f99185b8c6639b5b3db12a8dc820b8
1026639 <ivanhughes5604@icloud>
2023-09-15 08:55:00 -0400

    overloaded function

--- Exercises/ch05/overload.cpp
+++ Exercises/ch05/overload.cpp
@@ -2,46 +2,46 @@
 #include <cmath>
 using namespace std;
 
-double thing(double a){
+double overloaded_function(double a){
     double ans = a * 3;
     return ans;
 }
 
-int thing(int a, int b){
+int overloaded_function(int a, int b){
    int c= a + b;
     return c;
 }
 
-int thing(int a, int b, int c){
+int overloaded_function(int a, int b, int c){
    int d= a + b + c;
     return d;
 }
 
-double thing(double a, double b){
+double overloaded_function(double a, double b){
    double c= a +b;
     return c;
 }
 
-double thing(double a, double b, double c){
+double overloaded_function(double a, double b, double c){
    double d= a+b+c;
     return d;
 }
 
 int main(){
 
-double v = thing(4.0);
+double v = overloaded_function(4.0);
 cout << 4.0 << " tripled is " << v << endl;
 
-double s=thing(4,9);
+double s=overloaded_function(4,9);
 cout << "The sum is " << s << endl;
 
-int su=thing(2,3,4);
+int su=overloaded_function(2,3,4);
 cout << "The sum is "<< su << endl;
 
-double sum=thing(4.8,9.2);
+double sum=overloaded_function(4.8,9.2);
 cout << "The sum is " << sum << endl;
 
-double summ=thing(4.8,9.2,2.7);
+double summ=overloaded_function(4.8,9.2,2.7);
 cout << "The sum is " << summ << endl;
 
 return 0;
