26f2eeb2509776107d29cb672c61a1535ed0676c
1026639 <ivanhughes5604@icloud>
2023-09-15 09:16:43 -0400

    squares table

---
+++ Exercises/ch06/logtable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+double x = 1.0;
+while (x < 66536.0) {
+    cout << x << "\t" << log(x) / log(2.0) << endl;
+    x = x * 2.0;
+}
+}
