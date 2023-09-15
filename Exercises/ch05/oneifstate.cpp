579fd99bc165ac518c7a1e07b70c89b46fcc30fd
1026639 <ivanhughes5604@icloud>
2023-09-15 08:05:00 -0400

    rewritten if statement

---
+++ Exercises/ch05/oneifstate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+int main(){
+    int age = 40;
+    if ((age > 16) && (age < 65)) {
+   
+        cout << "age is within the normal working age." << endl;
+}   
+    return 0;
+}
