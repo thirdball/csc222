48e4051ea087f604b814f81584f7fb4f33f61f74
thirdball <ivanhughes5604@icloud.com>
2023-09-22 16:44:00 -0400

    counts letters

--- Exercises/ch07/Count_letters.cpp
+++ Exercises/ch07/Count_letters.cpp
@@ -2,14 +2,18 @@
 #include <string>
 using namespace std;
 
+int main(){
 string state = "Mississippi";
+char a = 's';
+
 int count = 0;
 int index = 0;
 
 while (index < state.length()) {
-    if (state[index] == 'i') {
+    if (state[index] == a) {
         count = count + 1;
     }
     index = index + 1;
 }
 cout << count << endl;
+}
