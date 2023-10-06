14944d49e5405069ea3e9165e542fad887d22364
thirdball <ivanhughes5604@icloud.com>
2023-10-06 08:24:39 -0400

    added readme file

--- Exercises/Comp_Problem/CompProblem.cpp
+++ Exercises/Comp_Problem/CompProblem.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 int main() {
     
-    vector<int> numbers = {8, 2, 2, 7, 3, 3, 3, 4, 5, 5, 67, 4, 5};
-    int consecutiveCount = 1;  // Start with 1 to account for the first occurrence
-    int targetNumber = numbers[0];  // Initialize with the first number
+    vector<int> numbers = {8, 2, 2, 7, 3, 3, 3, 4, 5, 5, 67, 67, 67, 67, 4, 5};
+    int ConsecutiveCount = 1;  // Start with 1 to account for the first occurrence
+    int TargetNumber = numbers[0];  // Initialize with the first number
 
     for (int i = 1; i < numbers.size(); i++) {
-        if (numbers[i] == targetNumber) {
-            consecutiveCount++;
-            if (consecutiveCount == 3) {
-                cout << "Consecutive values fround for " << targetNumber << "." << endl;
+        if (numbers[i] == TargetNumber) {
+            ConsecutiveCount++;
+            if (ConsecutiveCount == 3) {
+                cout << "Consecutive values found for " << TargetNumber << "." << endl;
                 break;
             }
         } else {
-            consecutiveCount = 1;  // Reset consecutive count if the current number is different
-            targetNumber = numbers[i];  // Update the target number
+            ConsecutiveCount = 1;  // Reset consecutive count if the current number is different
+            TargetNumber = numbers[i];  // Update the target number
         }
     }
 
-    if (consecutiveCount < 3) {
+    if (ConsecutiveCount < 3) {
         cout << "Consecutive numbers not found" << endl;
     }
 
---
+++ Exercises/Comp_Problem/README.md
@@ -0,0 +1,8 @@
+In this problem you are given a list of positive and negative integer values (excluding zero). You must determine if the same value was entered 3 times in a row any place in the list of integers. For example, the list:
+2 4 5 5 5 7 5 8 8 0
+
+Output would be:
+Consecutive values found for 5
+
+If not, the output should be:
+Consecutive values not found
