942a981623cc28f1788b6a96fc27e85c7fac7489
thirdball <ivanhughes5604@icloud.com>
2023-10-23 17:41:41 -0400

    Func without iteration

--- Exercises/ch12/EffIncFunc.cpp
+++ Exercises/ch12/EffIncFunc.cpp
@@ -7,29 +7,29 @@ struct Time {
     double second;
 
     void adjust_time();
+    void print_time();
 };
 
 void Time::adjust_time() {
-        int ExtraMin = static_cast<int>(second / 60);
-        second -= ExtraMin * 60;
-        minute += ExtraMin;
+
+    int ExtraMin = static_cast<int>(second / 60);
+    second -= ExtraMin * 60;
+    minute += ExtraMin;
         
-        int ExtraHours = minute / 60;
-        minute %= 60;
-        hour += ExtraHours;
+    int ExtraHours = minute / 60;
+    minute %= 60;
+    hour += ExtraHours;
     }
+
+void Time::print_time(){
+    cout << hour << ":" << minute << ":" << second << endl;
+}
 
 int main() {
 
 Time current_time = {5, 30 , 100.0};
 current_time.adjust_time();
-
-cout << "The time is " << current_time.hour << " hours, " 
-<< current_time.minute << " minutes, " << current_time.second 
-<< " seconds." << endl;
+current_time.print_time();
 
     return 0;
 }
-
-
-
